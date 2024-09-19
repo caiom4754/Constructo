65c79ed2cad72ae01be62d6fb03f5d0058bb218f
Dry <81354070+caiom4754@users.noreply.github.com>
2024-09-19 00:21:20 -0300

    0.2.3
    - Correção nos cálculos
    - possibilidade de nomear projeto

--- script.js
+++ script.js
@@ -71,106 +71,93 @@ function pararDesenho() {
 }
 
 // função para calcular a área da parede
-function calcularAreaParede() {
+function calcularComprimentoParede() {
     const tamanhoQuadrado = 40;
-    let area = 0;
+    let linear = 0;
     for (let i = 0; i < stack.length; i++) {
         const { startX, startY, endX, endY } = stack[i];
 
         // meu deusn não acredito quqe to tendo que usar teorema de pitagoras pra calcular pixel 
         const comprimento = Math.sqrt(Math.pow(endX - startX, 2) + Math.pow(endY - startY, 2)) / tamanhoQuadrado;
 
-        area += comprimento; // somando o comprimento da linha convertida para metros
+        linear += comprimento; // somando o comprimento da linha convertida para metros
     }
-    return area;
+    return linear;
 }
 
-// função para calcular o numero de blocos
+// Função para calcular o número de blocos
 function calcularNumeroBlocos(alturaParede) {
     const tipoBloco = document.getElementById('tipoBloco').value;
     let dimensoesBloco;
 
     switch (tipoBloco) {
-        case 'concreto39x19x14':
-            dimensoesBloco = { comprimento: 0.39, altura: 0.19 };
+        case 'concreto34x19x14':
+            dimensoesBloco = { comprimento: 0.34, altura: 0.19, largura: 0.14 };
             break;
-        case 'ceramico19x19':
-            dimensoesBloco = { comprimento: 0.19, altura: 0.19 };
-            break;
-        case 'ceramicoEstrutural29x19':
-            dimensoesBloco = { comprimento: 0.29, altura: 0.19 };
-            break;
-        case 'concretoCelular60x20':
-            dimensoesBloco = { comprimento: 0.60, altura: 0.20 };
-            break;
-        case 'silicioCalcio25x20':
-            dimensoesBloco = { comprimento: 0.25, altura: 0.20 };
+        case 'ceramico19x19x09':
+            dimensoesBloco = { comprimento: 0.19, altura: 0.19, largura: 0.09 };
             break;
         default:
             alert("Tipo de bloco não reconhecido!");
             return;
     }
 
-    const areaParede = calcularAreaParede() * alturaParede;
+    const areaParede = calcularComprimentoParede() * alturaParede;
     const areaBloco = dimensoesBloco.comprimento * dimensoesBloco.altura;
     const numeroBlocos = Math.ceil(areaParede / areaBloco);
     return numeroBlocos;
 }
 
-// função para calcular materiaia
-function calcularMateriais(alturaParede, numeroBlocos) {
+// Função para calcular materiais
+function calcularMateriais(alturaParede) {
+
+    // levando em consideração que as medidas serão 1:4:.5 (cimento:areia:agua)
+
     const tipoBloco = document.getElementById('tipoBloco').value;
-    const areaParede = calcularAreaParede() * alturaParede;
 
-    // qiantidade de materiais por m² para cada tipo de bloco
-    let cimentoPorM2, areiaPorM2, calPorM2, aguaPorM2;
+    // Chamando a função calcularNumeroBlocos e armazenando o resultado
+    const numBlocos = calcularNumeroBlocos(alturaParede);
+    // chamando a função para pegar a área da parede
+    const areaParede = calcularComprimentoParede() * alturaParede;
 
-    switch (tipoBloco) {
-        case 'concreto39x19x14':
-            cimentoPorM2 = 0.12; // sacos de cimento (50kg) por m²
-            areiaPorM2 = 0.04; // metros cúbicos de areia por m²
-            calPorM2 = 0.03; // metros cúbicos de cal por m²
-            aguaPorM2 = 18; // litros de água por m²
-            break;
-        case 'ceramico19x19':
-            cimentoPorM2 = 0.10;
-            areiaPorM2 = 0.05;
-            calPorM2 = 0.04;
-            aguaPorM2 = 15;
-            break;
-        case 'ceramicoEstrutural29x19':
-            cimentoPorM2 = 0.08;
-            areiaPorM2 = 0.06;
-            calPorM2 = 0.03;
-            aguaPorM2 = 12;
-            break;
-        case 'concretoCelular60x20':
-            cimentoPorM2 = 0.14;
-            areiaPorM2 = 0.03;
-            calPorM2 = 0.05;
-            aguaPorM2 = 20;
-            break;
-        case 'silicioCalcio25x20':
-            cimentoPorM2 = 0.11;
-            areiaPorM2 = 0.04;
-            calPorM2 = 0.04;
-            aguaPorM2 = 16;
-            break;
-        default:
-            cimentoPorM2 = 0;
-            areiaPorM2 = 0;
-            calPorM2 = 0;
-            aguaPorM2 = 0;
-            break;
+    if (!numBlocos) {
+        return { cimento: 0 }; // Retorna 0 caso o cálculo de blocos falhe
     }
 
+    let blocosPorSaco;
+
+    switch (tipoBloco) {
+        case 'concreto34x19x14':
+            blocosPorSaco = 150; // 1 saco de cimento para 150 blocos de concreto
+            areiaPorBloco = 0.020;
+            break;
+        case 'ceramico19x19x09':
+            blocosPorSaco = 200; // 1 saco de cimento para 200 blocos cerâmicos
+            areiaPorBloco = 0.015;
+            break;
+        default:
+            return { cimento: 0, areia: 0 };
+    }
+
+    // Calculando o número de sacos de cimento
+    const sacosCimento = (numBlocos / blocosPorSaco);
+
+    // Calculando a quantidade de areia
+    const areiaPorM3 = areaParede * 0.020;
+
+    //calcular a quantidade de awa
+    const aguaLitros = (sacosCimento * 50)*0.5;
+
+    //heheboy tudp funfando
+
     return {
-        cimento: (cimentoPorM2 * areaParede).toFixed(2),
-        areia: (areiaPorM2 * areaParede).toFixed(2),
-        cal: (calPorM2 * areaParede).toFixed(2),
-        agua: (aguaPorM2 * areaParede).toFixed(2)
+        cimento: sacosCimento.toFixed(2),
+        areia: areiaPorM3.toFixed(2),
+        agua: aguaLitros.toFixed(1)
     };
 }
+
+
 
 // função para exibir o número de blocos e materiais
 function exibirResultados(numeroBlocos, materiais) {
@@ -181,7 +168,6 @@ function exibirResultados(numeroBlocos, materiais) {
             <li>Blocos: ${numeroBlocos} </li>
             <li>Cimento: ${materiais.cimento} sacos (50kg)</li>
             <li>Areia: ${materiais.areia} m³</li>
-            <li>Cal: ${materiais.cal} m³</li>
             <li>Água: ${materiais.agua} litros</li>
         </ul>
     `;
@@ -210,31 +196,47 @@ botaoCalcular.addEventListener('click', function () {
     exibirResultados(numeroBlocos, materiais);
 });
 
-// adicionar eventos de mouse para desenhar
+// adicionar eventos do mouse para desenhar
 canvas.addEventListener('mousedown', comecarDesenho);
 canvas.addEventListener('mousemove', desenhar);
 canvas.addEventListener('mouseup', pararDesenho);
 canvas.addEventListener('mouseout', pararDesenho);
 
-// Função para salvar o projeto em um arquivo JSON
+// Função para normaizar o nome do projeto
+function limparNomeProjeto(nome) {
+    //remove os acentos
+    const nomeSemAcentos = nome.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+
+    //substitui espaços por - 
+    const nomeSemEspacos = nomeSemAcentos.replace(/\s+/g, '-');
+
+    //remove os caracteres especiais 
+    const nomeLimpo = nomeSemEspacos.replace(/[^a-zA-Z0-9-]/g, '');
+
+    //converte tudo para letra minuscula
+    return nomeLimpo.toLowerCase();
+}
+
+// função para salvar o projeto em um arquivo JSON
 function baixarProjeto() {
     const alturaParede = document.getElementById('alturaParede').value;
     const tipoBloco = document.getElementById('tipoBloco').value;
-
+    const nomeProjetoInput = document.getElementById('nomeProjeto').value;
+    const nomeArquivo = limparNomeProjeto(nomeProjetoInput);
     const projeto = {
         alturaParede: alturaParede,
         tipoBloco: tipoBloco,
-        stack: stack, // Salva as coordenadas do desenho
+        stack: stack, // salva as cooedenadas do desenho
     };
 
     const blob = new Blob([JSON.stringify(projeto)], { type: 'application/json' });
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
-    link.download = 'projeto.json';
+    link.download = nomeArquivo + '.json';
     link.click();
 }
 
-// Função para carregar o projeto de um arquivo JSON
+// função para carregar o projeto com JSON
 function carregarProjetoDoArquivo(event) {
     const file = event.target.files[0];
     const reader = new FileReader();
@@ -242,13 +244,13 @@ function carregarProjetoDoArquivo(event) {
     reader.onload = function (e) {
         const projeto = JSON.parse(e.target.result);
 
-        // Atualiza os campos de altura e tipo de bloco
+        // atualiza os campos de altura e tipo de bloco
         document.getElementById('alturaParede').value = projeto.alturaParede;
         document.getElementById('tipoBloco').value = projeto.tipoBloco;
 
-        // Recarrega as coordenadas do desenho
+        // recarrega as cooedenadas do desenho
         stack = projeto.stack;
-        redesenhar(); // Redesenha o canvas com as linhas salvas
+        redesenhar();
 
         alert('Projeto carregado com sucesso!');
     };
@@ -256,12 +258,12 @@ function carregarProjetoDoArquivo(event) {
     reader.readAsText(file);
 }
 
-// Função para abrir o seletor de arquivo JSON
+// função para abrir o seletor de arquivo JSON
 function abrirSeletorArquivo() {
     document.getElementById('carregarArquivoInput').click();
 }
 
-// Adicionar eventos aos botões
+// adcicionar eventos aos botões
 document.getElementById('baixarProjetoButton').addEventListener('click', baixarProjeto);
 document.getElementById('carregarArquivoInput').addEventListener('change', carregarProjetoDoArquivo);
 document.getElementById('carregarArquivoButton').addEventListener('click', abrirSeletorArquivo);
